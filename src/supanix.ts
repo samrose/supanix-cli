1e05281931b59ac0cdf49d22fb938a2280381ab7
samrose <samuel.rose@gmail.com>
2024-03-12 18:14:59 -0400

    feat: color

--- src/supanix.ts
+++ src/supanix.ts
@@ -1,18 +1,20 @@
-const figlet = require("figlet");
+const figlet = require('figlet');
 
-console.log(figlet.textSync("SUPANIX"));
-
-
-const { Command } = require("commander");
+const { Command } = require('commander');
 const { exec } = require('child_process');
+const { yellow, red, cyan, blue, green } = require('kleur');
+console.log(cyan("Welcome to SUPANIX!"));
+
+console.log(green(figlet.textSync("SUPANIX")));
+
 const program = new Command();
 
 program
   .command('build <flakeURL>')
   .description('Run the nix build command on the system')
-  .action(() => {
+  .action((flakeURL: string) => {
     // Execute the ls command using child_process.exec
-    exec('nix build ${flakeURL}', ((err: Error | null, stdout: string, stderr: string) => {
+    exec(`nix build ${flakeURL}`, ((err: Error | null, stdout: string, stderr: string) => {
         if (err) {
             console.error(`exec error: ${err}`);
             return;
